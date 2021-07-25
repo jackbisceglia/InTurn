e8076c1cfc5baf20ee8a4f7a84b3477faed8f83f
ewu2023 <ericwu2019@gmail.com>
2021-07-24 20:48:27 -0400

    Updated hidden

--- Monitor/hidden.py
+++ Monitor/hidden.py
@@ -1,5 +1,1 @@
-BASE_URL = 'https://apptrack-backend.ue.r.appspot.com'
 
-TWILIO_API_KEY = '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'
-
-GITHUB_API_KEY = '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'
