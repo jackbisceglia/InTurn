615f4b7a7e539e27954c21e01e9d7a1b5bd20b6d
jackbisceglia <jackbisceglia2000@gmail.com>
2021-07-27 16:39:51 -0400

    updated title, added logo, and fixed favicon/metadata

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,12 @@
+import '../styles/Entry.css'
+
 function Header() {
     return (
         <div className="header-wrapper">
-            <h1 className="header-title">PITT CSC INTERNSHIP TRACKER</h1>
+            <div className="head">
+                <img className="icon" src="./imgs/code.svg" alt="" />
+                <h1 className="header-title">InTurn</h1>
+            </div>
             <p>created by <a rel="noreferrer" target="_blank" href="https://github.com/jackbisceglia">Jack Bisceglia</a>, <a rel="noreferrer" target="_blank" href="https://github.com/joepetrillo">Joe Petrillo</a>, <a rel="noreferrer" target="_blank" href="https://github.com/ewu2023">Eric Wu</a> and <a rel="noreferrer" target="_blank" href="https://github.com/sid2033">Siddharth Raju</a></p>
         </div>
     );
